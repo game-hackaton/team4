854c1b6a1723b4d497aa40ebe5ea5dc8021a0bbb
dragInSky <cronosdversus@mail.ru>
2024-03-30 14:49:30 +0500

    Fast fix

--- src/Services/TestData.cs
+++ src/Services/TestData.cs
@@ -10,55 +10,55 @@ public class TestData
     {
         var width = 10;
         var height = 8;
-        var testCells = new CellDto[10 * 10];
-        // {
-        //     // new CellDto("100", new VectorDto {X = 0, Y = 0}, "wall", "", 0),
-        //     // new CellDto("101", new VectorDto {X = 0, Y = 1}, "wall", "", 0),
-        //     // new CellDto("110", new VectorDto {X = 0, Y = 2}, "wall", "", 0),
-        //     // new CellDto("102", new VectorDto {X = 0, Y = 3}, "wall", "", 0),
-        //     // new CellDto("103", new VectorDto {X = 0, Y = 4}, "wall", "", 0),
-        //     // new CellDto("104", new VectorDto {X = 0, Y = 5}, "wall", "", 0),
-        //     // new CellDto("105", new VectorDto {X = 1, Y = 0}, "wall", "", 0),
-        //     // new CellDto("106", new VectorDto {X = 2, Y = 0}, "wall", "", 0),
-        //     // new CellDto("107", new VectorDto {X = 3, Y = 0}, "wall", "", 0),
-        //     // new CellDto("108", new VectorDto {X = 4, Y = 0}, "wall", "", 0),
-        //     // new CellDto("109", new VectorDto {X = 5, Y = 0}, "wall", "", 0),
-        // new CellDto("100", new VectorDto {X = 2, Y = 4}, "color1", "", 0),
-        // new CellDto("200", new VectorDto {X = 5, Y = 4}, "color1", "", 0),
-        // new CellDto("300", new VectorDto {X = 3, Y = 1}, "color2", "", 20),
-        // new CellDto("400", new VectorDto {X = 1, Y = 1}, "color2", "", 20),
-        // new CellDto("500", movingObjectPosition, "player", "", 10), //☺️
-        // };
-        testCells[0] = new CellDto("100", new VectorDto { X = 2, Y = 4 }, "color1", "", 0);
-        testCells[1] = new CellDto("200", new VectorDto { X = 5, Y = 4 }, "color1", "", 0);
-        testCells[2] = new CellDto("300", new VectorDto { X = 3, Y = 1 }, "color2", "", 20);
-        testCells[3] = new CellDto("400", new VectorDto { X = 1, Y = 1 }, "color2", "", 20);
-        testCells[4] = new CellDto("500", movingObjectPosition, "player", "", 10);
-        int index = 5;
-        AddWallX(height, 0);
-        AddWallX(height, width);
-        AddWallY(width, 0);
-        AddWallY(width, height);
-
+        var testCells = new[]
+        {
+            //     // new CellDto("100", new VectorDto {X = 0, Y = 0}, "wall", "", 0),
+            //     // new CellDto("101", new VectorDto {X = 0, Y = 1}, "wall", "", 0),
+            //     // new CellDto("110", new VectorDto {X = 0, Y = 2}, "wall", "", 0),
+            //     // new CellDto("102", new VectorDto {X = 0, Y = 3}, "wall", "", 0),
+            //     // new CellDto("103", new VectorDto {X = 0, Y = 4}, "wall", "", 0),
+            //     // new CellDto("104", new VectorDto {X = 0, Y = 5}, "wall", "", 0),
+            //     // new CellDto("105", new VectorDto {X = 1, Y = 0}, "wall", "", 0),
+            //     // new CellDto("106", new VectorDto {X = 2, Y = 0}, "wall", "", 0),
+            //     // new CellDto("107", new VectorDto {X = 3, Y = 0}, "wall", "", 0),
+            //     // new CellDto("108", new VectorDto {X = 4, Y = 0}, "wall", "", 0),
+            //     // new CellDto("109", new VectorDto {X = 5, Y = 0}, "wall", "", 0),
+            new CellDto("100", new VectorDto { X = 2, Y = 4 }, "wall", "", 0),
+            new CellDto("200", new VectorDto { X = 5, Y = 4 }, "wall", "", 0),
+            new CellDto("300", new VectorDto { X = 3, Y = 1 }, "box", "", 20),
+            new CellDto("400", new VectorDto { X = 6, Y = 1 }, "box", "", 20),
+            new CellDto("500", movingObjectPosition, "player", "", 10), //☺️
+        };
+        // testCells[0] = new CellDto("100", new VectorDto { X = 2, Y = 4 }, "color1", "", 0);
+        // testCells[1] = new CellDto("200", new VectorDto { X = 5, Y = 4 }, "color1", "", 0);
+        // testCells[2] = new CellDto("300", new VectorDto { X = 3, Y = 1 }, "color2", "", 20);
+        // testCells[3] = new CellDto("400", new VectorDto { X = 1, Y = 1 }, "color2", "", 20);
+        // testCells[4] = new CellDto("500", movingObjectPosition, "player", "", 10);
+        // int index = 5;
+        // AddWallX(height, 0);
+        // AddWallX(height, width);
+        // AddWallY(width, 0);
+        // AddWallY(width, height);
+        //
         return new GameDto(testCells, true, true, width, height, Guid.Empty, movingObjectPosition.X == 0,
             movingObjectPosition.Y);
-
-        void AddWallX(int j, int y)
-        {
-            for (int i = 0; i < j; i++)
-            {
-                testCells[index] = new CellDto((index).ToString(), new VectorDto { X = i, Y = y }, "color1", "", 0);
-                index++;
-            }
-        }
-
-        void AddWallY(int j, int x)
-        {
-            for (int i = 0; i < j; i++)
-            {
-                testCells[index] = new CellDto((index).ToString(), new VectorDto { X = x, Y = i }, "color1", "", 0);
-                index++;
-            }
-        }
+        //
+        // void AddWallX(int j, int y)
+        // {
+        //     for (int i = 0; i < j; i++)
+        //     {
+        //         testCells[index] = new CellDto((index).ToString(), new VectorDto { X = i, Y = y }, "color1", "", 0);
+        //         index++;
+        //     }
+        // }
+        //
+        // void AddWallY(int j, int x)
+        // {
+        //     for (int i = 0; i < j; i++)
+        //     {
+        //         testCells[index] = new CellDto((index).ToString(), new VectorDto { X = x, Y = i }, "color1", "", 0);
+        //         index++;
+        //     }
+        // }
     }
 }
