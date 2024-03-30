64ccabbcb20013d8686305a4aec775a232cb2ba5
dragInSky <cronosdversus@mail.ru>
2024-03-30 16:06:05 +0500

    Done map

--- src/Services/TestData.cs
+++ src/Services/TestData.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using thegame.Models;
 
 namespace thegame.Services;
@@ -10,55 +10,56 @@ public class TestData
     {
         var width = 10;
         var height = 8;
-        var testCells = new[]
-        {
-            //     // new CellDto("100", new VectorDto {X = 0, Y = 0}, "wall", "", 0),
-            //     // new CellDto("101", new VectorDto {X = 0, Y = 1}, "wall", "", 0),
-            //     // new CellDto("110", new VectorDto {X = 0, Y = 2}, "wall", "", 0),
-            //     // new CellDto("102", new VectorDto {X = 0, Y = 3}, "wall", "", 0),
-            //     // new CellDto("103", new VectorDto {X = 0, Y = 4}, "wall", "", 0),
-            //     // new CellDto("104", new VectorDto {X = 0, Y = 5}, "wall", "", 0),
-            //     // new CellDto("105", new VectorDto {X = 1, Y = 0}, "wall", "", 0),
-            //     // new CellDto("106", new VectorDto {X = 2, Y = 0}, "wall", "", 0),
-            //     // new CellDto("107", new VectorDto {X = 3, Y = 0}, "wall", "", 0),
-            //     // new CellDto("108", new VectorDto {X = 4, Y = 0}, "wall", "", 0),
-            //     // new CellDto("109", new VectorDto {X = 5, Y = 0}, "wall", "", 0),
-            new CellDto("100", new VectorDto { X = 2, Y = 4 }, "wall", "", 0),
-            new CellDto("200", new VectorDto { X = 5, Y = 4 }, "wall", "", 0),
-            new CellDto("300", new VectorDto { X = 3, Y = 1 }, "box", "", 20),
-            new CellDto("400", new VectorDto { X = 6, Y = 1 }, "box", "", 20),
-            new CellDto("500", movingObjectPosition, "player", "", 10), //☺️
-        };
-        // testCells[0] = new CellDto("100", new VectorDto { X = 2, Y = 4 }, "color1", "", 0);
-        // testCells[1] = new CellDto("200", new VectorDto { X = 5, Y = 4 }, "color1", "", 0);
-        // testCells[2] = new CellDto("300", new VectorDto { X = 3, Y = 1 }, "color2", "", 20);
-        // testCells[3] = new CellDto("400", new VectorDto { X = 1, Y = 1 }, "color2", "", 20);
-        // testCells[4] = new CellDto("500", movingObjectPosition, "player", "", 10);
-        // int index = 5;
-        // AddWallX(height, 0);
-        // AddWallX(height, width);
-        // AddWallY(width, 0);
-        // AddWallY(width, height);
-        //
+        var testCells = new CellDto[8 * 10];
+        testCells[0] = new CellDto("0", new VectorDto { X = 2, Y = 4 }, "box", "", 0);
+        testCells[1] = new CellDto("1", new VectorDto { X = 4, Y = 5 }, "box", "", 0);
+        
+        testCells[2] = new CellDto("2", new VectorDto { X = 3, Y = 1 }, "target", "", 20);
+        testCells[3] = new CellDto("3", new VectorDto { X = 7, Y = 6 }, "target", "", 20);
+        
+        testCells[4] = new CellDto("4", movingObjectPosition, "player", "", 10);
+        
+        //target id: 3
+        testCells[5] = new CellDto("5", new VectorDto { X = 7, Y = 5 }, "wall", "", 0);
+        testCells[6] = new CellDto("6", new VectorDto { X = 6, Y = 6 }, "wall", "", 0);
+        
+        //random
+        testCells[7] = new CellDto("7", new VectorDto { X = 3, Y = 6 }, "wall", "", 0);
+        testCells[8] = new CellDto("8", new VectorDto { X = 6, Y = 6 }, "wall", "", 0);
+        
+        //random
+        testCells[9] = new CellDto("8", new VectorDto { X = 3, Y = 3 }, "wall", "", 0);
+        
+        //target id:2
+        testCells[10] = new CellDto("7", new VectorDto { X = 2, Y = 2 }, "wall", "", 0);
+        testCells[11] = new CellDto("8", new VectorDto { X = 3, Y = 3 }, "wall", "", 0);
+        
+        int index = testCells.Count(s => s != null);
+        AddWallX(10, 0);
+        AddWallY(7, 0);
+
+        AddWallX(10, 7);
+        AddWallY(7, 9);
+
         return new GameDto(testCells, true, true, width, height, Guid.Empty, movingObjectPosition.X == 0,
             movingObjectPosition.Y);
-        //
-        // void AddWallX(int j, int y)
-        // {
-        //     for (int i = 0; i < j; i++)
-        //     {
-        //         testCells[index] = new CellDto((index).ToString(), new VectorDto { X = i, Y = y }, "color1", "", 0);
-        //         index++;
-        //     }
-        // }
-        //
-        // void AddWallY(int j, int x)
-        // {
-        //     for (int i = 0; i < j; i++)
-        //     {
-        //         testCells[index] = new CellDto((index).ToString(), new VectorDto { X = x, Y = i }, "color1", "", 0);
-        //         index++;
-        //     }
-        // }
+
+        void AddWallX(int j, int y)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                testCells[index] = new CellDto((index).ToString(), new VectorDto { X = i, Y = y }, "wall", "", 0);
+                index++;
+            }
+        }
+
+        void AddWallY(int j, int x)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                testCells[index] = new CellDto((index).ToString(), new VectorDto { X = x, Y = i }, "wall", "", 0);
+                index++;
+            }
+        }
     }
 }
